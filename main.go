894a19cb02a5e55cfa32ee58f98b3d69904c10c3
shinthanthtun <shinthanthtun00@gmail.com>
2024-01-14 10:45:54 +0630

    Loop added

--- main.go
+++ main.go
@@ -1,47 +1,43 @@
 package main
 
-import(
-  "fmt"
+import (
+	"fmt"
 )
 
-func main(){
-  var conferenceName string = "Music Conference"
-  const conferenceTicket int = 50
-  var remainingTicket uint = 50
+func main() {
+	var conferenceName string = "Music Conference"
+	const conferenceTicket int = 50
+	var remainingTicket uint = 50
+	var bookings []string
 
+	fmt.Printf("Welcome to %v booking application!!!\n", conferenceName)
+	fmt.Printf("We have total of %v tickens and %v are still available.\n", conferenceName, remainingTicket)
+	fmt.Println("Get your tickets here to attend")
 
-  fmt.Printf("Welcome to %v booking application!!!\n" , conferenceName)
-  fmt.Printf("We have total of %v tickens and %v are still available.\n", conferenceName , remainingTicket)
-  fmt.Println("Get your tickets here to attend")
-  
-  var bookings [50]string
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-  var firstName string
-  var lastName string
-  var email string
-  var userTickets uint
-  //ask user for their tickets
-  fmt.Print("What's your first name : ")
-  fmt.Scan(&firstName)
+		//ask user for their tickets
+		fmt.Print("What's your first name : ")
+		fmt.Scan(&firstName)
 
-  fmt.Print("What's your last name : ")
-  fmt.Scan(&lastName)
+		fmt.Print("What's your last name : ")
+		fmt.Scan(&lastName)
 
-  fmt.Print("What's your email : ")
-  fmt.Scan(&email)
+		fmt.Print("What's your email : ")
+		fmt.Scan(&email)
 
-  fmt.Print("How many ticket do you wanna book : ")
-  fmt.Scan(&userTickets)
+		fmt.Print("How many ticket do you wanna book : ")
+		fmt.Scan(&userTickets)
 
-  remainingTicket = remainingTicket - userTickets
-  bookings[0] = firstName + " " + lastName
+		remainingTicket = remainingTicket - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-  fmt.Printf("The whole array : %v\n" , bookings)
-  fmt.Printf("The first value : %v\n" , bookings)
-
-  fmt.Printf("Thank you %v %v for booking.\n" , firstName, lastName)
-  fmt.Printf("You will reveice a comfirmation email at your mail address %v\n" , email)
-
-  
+		fmt.Printf("Thank you %v %v for booking.\n", firstName, lastName)
+		fmt.Printf("You will reveice a comfirmation email at your mail address %v\n", email)
+		fmt.Printf("These are all our bookings : %v\n", bookings)
+	}
 }
-
