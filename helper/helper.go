7f7df6b9af7f2572291c5103135b7620470530da
shinthanthtun <shinthanthtun00@gmail.com>
2024-01-15 23:40:04 +0630

    Finally fix the error

--- helper/helper.go
+++ helper/helper.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 )
 
-func ValidateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTicket uint) (bool, bool, bool) {
+func ValidateUserInput(firstName string, lastName string, email string, userTickets int, remainingTicket int) (bool, bool, bool) {
 	isValidName := len(firstName) > 2 && len(lastName) > 2
 	isValidEmail := strings.Contains(email, "@")
 	isValidTickets := userTickets > 0 && userTickets <= remainingTicket
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 
 	const conferenceTicket int = 50
 
-	var remainingTicket uint = 50
+	var remainingTicket int = 50
 	var bookings []string
 	for remainingTicket > 0 && len(bookings) < 50 {
 		GreetUser(conferenceName, conferenceTicket, remainingTicket)
@@ -21,7 +21,7 @@ func main() {
 		isValidName, isValidEmail, isValidTickets := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTicket)
 
 		if isValidName && isValidEmail && isValidTickets {
-			BookTickets(remainingTicket, userTickets, bookings, firstName, lastName, email)
+			bookings := BookTickets(&remainingTicket, &userTickets, bookings, firstName, lastName, email)
 			//Print first name function
 			firstNames := PrintFirstName(bookings)
 			fmt.Printf("These are all our bookings : %v\n", firstNames)
@@ -46,13 +46,13 @@ func main() {
 	}
 }
 
-func BookTickets(remainingTicket uint, userTickets uint, bookings []string, firstName string, lastName string, email string) {
-	remainingTicket = remainingTicket - userTickets
+func BookTickets(remainingTicket *int, userTickets *int, bookings []string, firstName string, lastName string, email string) []string {
+	*remainingTicket = *remainingTicket - *userTickets
 	bookings = append(bookings, firstName+" "+lastName)
 
 	fmt.Printf("Thank you %v %v for booking.\n", firstName, lastName)
 	fmt.Printf("You will reveice a comfirmation email at your mail address %v\n", email)
-
+	return bookings
 }
 
 func PrintFirstName(bookings []string) []string {
@@ -64,17 +64,17 @@ func PrintFirstName(bookings []string) []string {
 	return firstNames
 }
 
-func GreetUser(conferenceName string, conferenceTicket int, remainingTicket uint) {
+func GreetUser(conferenceName string, conferenceTicket int, remainingTicket int) {
 	fmt.Printf("\nWelcome to %v booking application!!!\n", conferenceName)
 	fmt.Printf("We have total of %v tickens and %v are still available.\n", conferenceTicket, remainingTicket)
 	fmt.Println("Get your tickets here to attend")
 }
 
-func GetUserInput() (string, string, string, uint) {
+func GetUserInput() (string, string, string, int) {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets uint
+	var userTickets int
 
 	//ask user for their tickets
 	fmt.Print("What's your first name : ")
